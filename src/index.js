65fae6363cb03ed6b6debddb442ee9e4c0b99fc3
Joy Bappy <joybappy2025@gmail.com>
2026-06-21 21:53:08 +0600

    add json parsing middleware

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const db = await connectDB();
 const usersCollection = db.collection("users");
 
 app.get("/", (req, res) => res.send("Hello From Server"));
 
-app.post("/joy", (req, res) => {
-  res.status(200).json({ message: "Hello" });
+app.post("/users", async (req, res) => {
+  const newUser = req;
+  console.log(newUser);
+  //   const user = await usersCollection.insertOne(newUser);
+  //   res.status(200).json({ message: user });
 });
 
 app.listen(process.env.PORT, () => console.log("Server is up 🚀"));
