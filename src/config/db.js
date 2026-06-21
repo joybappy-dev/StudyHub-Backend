0f09e68346cf0dd6f508a8ab6480b0471a152194
Joy Bappy <joybappy2025@gmail.com>
2026-06-21 21:44:26 +0600

    return db from connectDB function to use db without model and schema

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,8 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ MongoDB connected Successfully");
+    const db = mongoose.connection.db;
+    return db;
   } catch (err) {
     console.log(`❌ ${err.message}`);
 
